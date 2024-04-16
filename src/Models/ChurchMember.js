238c475ea32823344fdd1fa6adfc1ee7d9b8d80e
Julius <juliusconstantindjossou@etudiant.ief2i.fr>
2024-04-16 06:57:21 +0200

    create churchmember model

---
+++ src/Models/ChurchMember.js
@@ -0,0 +1,18 @@
+class ChurchMember {
+    constructor(firstName, lastName, dateOfBirth, gender, address, phoneNumber, email, joinDate, maritalStatus, baptized, photoUrl) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.dateOfBirth = dateOfBirth;
+      this.gender = gender;
+      this.address = address;
+      this.phoneNumber = phoneNumber;
+      this.email = email;
+      this.joinDate = joinDate;
+      this.maritalStatus = maritalStatus;
+      this.baptized = baptized;
+      this.photoUrl = photoUrl;
+    }
+  }
+  
+  module.exports = ChurchMember;
+  
